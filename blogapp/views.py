a9ac0a851b258b43b5c6f8fbf93ec4eae35208c4
RutterFritzz <pps.rutger@gmail.com>
2026-05-18 15:06:00 +0200

    added style and simplistic urls

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.views import generic
 
 from .models import Blog
 
-def index(request):
-    blogs = Blog.objects.all()
-    return render(request, 'blogapp/index.html', {'blogs': blogs})
+class IndexView(generic.ListView):
+    model = Blog
+    template_name = 'blogapp/index.html'
+    context_object_name = 'blogs'
 
-def detail(request, blog_id):
-    blog = get_object_or_404(Blog, id=blog_id)
-    return render(request, 'blogapp/detail.html', {'blog': blog})
+class DetailView(generic.DetailView):
+    model = Blog
+    template_name = 'blogapp/detail.html'
