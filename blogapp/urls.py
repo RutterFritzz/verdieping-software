07bd197e3c395c54908e594cdd1415037a6ca35f
RutterFritzz <pps.rutger@gmail.com>
2026-05-19 14:29:31 +0200

    added loging in and out

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 app_name = 'blogapp'
 urlpatterns = [
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register_view, name="register"),
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("create/", views.create_blog, name="create_blog"),
     path("edit/<int:pk>/", views.edit_blog, name="edit_blog"),
     path("delete/<int:pk>/", views.delete_blog, name="delete_blog"),
+    path("profile/<int:pk>/", views.ProfileView.as_view(), name="profile"),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.views import generic
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 
 from blogapp.forms import BlogForm
 
@@ -15,6 +18,34 @@ class DetailView(generic.DetailView):
     model = Blog
     queryset = Blog.objects.select_related('author')
     template_name = 'blogapp/detail.html'
+
+class ProfileView(generic.DetailView):
+    model = User
+    template_name = 'blogapp/profile.html'
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('blogapp:index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blogapp/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('blogapp:index')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogapp:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blogapp/register.html', {'form': form})
 
 def create_blog(request):
     if request.method == 'POST':
